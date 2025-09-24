39e1a73ca1d3358a5bb4ade2b9bcc1c0d53b7eab
asus <asus@DESKTOP-1IHTQ4L>
2025-09-24 14:50:53 +0300

    Add Services Folder and Add Controller

---
+++ JadooTravel/Services/DestinationServices/IDestinationService.cs
@@ -0,0 +1,12 @@
+﻿using JadooTravel.Dtos.DestinationDtos;
+
+namespace JadooTravel.Services.DestinationServices;
+
+public interface IDestinationService
+{
+    Task<List<ResultDestinationDto>> GetDestinationListAsync();
+    Task CreateDestinationAsync(CreateDestinationDto model);
+    Task UpdateDestinationAsync(UpdateDestinationDto model);
+    Task DeleteDestinationAsync(string id);
+    Task<GetDestinationByIdDto> GetDestinationByIdAsync(string id);
+}
