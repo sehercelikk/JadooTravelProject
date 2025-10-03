41869259ec827527ee3b1329a3647eed3c7b532f
asus <asus@DESKTOP-1IHTQ4L>
2025-10-03 16:34:42 +0300

    .

--- JadooTravel/ViewComponents/_DefaultCategoryComponentPartial.cs
+++ JadooTravel/ViewComponents/_DefaultCategoryComponentPartial.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JadooTravel.Services.TranslatorService;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JadooTravel.ViewComponents;
 
 public class _DefaultCategoryComponentPartial : ViewComponent
 {
+    private readonly TranslatorService _translator;
+
+    public _DefaultCategoryComponentPartial(TranslatorService translator)
+    {
+        _translator = translator;
+    }
+
     public IViewComponentResult Invoke()
     {
         return View();
