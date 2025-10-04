abf6b9d2d2100264efafd3568af413f91d8254df
asus <asus@DESKTOP-1IHTQ4L>
2025-10-04 14:09:01 +0300

    add testimonial viewcomponent

--- JadooTravel/ViewComponents/_DefaultTestimonialComponentPartial.cs
+++ JadooTravel/ViewComponents/_DefaultTestimonialComponentPartial.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JadooTravel.Services.TestimonialService;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JadooTravel.ViewComponents;
 
 public class _DefaultTestimonialComponentPartial : ViewComponent
 {
+    private readonly ITestimonialService _testimonialService;
+
+    public _DefaultTestimonialComponentPartial(ITestimonialService testimonialService)
+    {
+        _testimonialService = testimonialService;
+    }
+
     public IViewComponentResult Invoke()
     {
         return View();
